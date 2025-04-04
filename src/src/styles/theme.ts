b3776215ae015a2d826ecd5c68012434cc39aa2c
eslley <rubemeslley@gmail.com>
2025-04-04 17:55:16 -0300

    feat: add theme provider

--- src/src/styles/theme.ts
+++ src/src/styles/theme.ts
@@ -1,11 +1,47 @@
-export const theme = {
-  primary: "#1976d2",
-  secondary: "#dc004e",
-  success: "#388e3c",
-  error: "#d32f2f",
-  warning: "#f57c00",
-  info: "#0288d1",
-  background: "#f4f6f8",
-  textPrimary: "#333",
-  textSecondary: "#666",
-};
+import { createTheme } from "@mui/material/styles";
+
+export enum ThemeColors {
+  primary = "#1d272a",
+  secondary = "#984B43",
+  success = "#388e3c",
+  error = "#d32f2f",
+  warning = "#eac67a",
+  info = "#0288d1",
+  background = "#18121E",
+  textPrimary = "#333",
+  textSecondary = "#666",
+}
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: ThemeColors.primary,
+    },
+    secondary: {
+      main: ThemeColors.secondary,
+    },
+    success: {
+      main: ThemeColors.success,
+    },
+    error: {
+      main: ThemeColors.error,
+    },
+    warning: {
+      main: ThemeColors.warning,
+    },
+    info: {
+      main: ThemeColors.info,
+    },
+    background: {
+      default: ThemeColors.background,
+    },
+    text: {
+      primary: ThemeColors.textPrimary,
+      secondary: ThemeColors.textSecondary,
+    },
+  },
+  typography: {
+    // Optional: Customize typography if needed
+    fontFamily: "Roboto, Arial, sans-serif", // Customize font
+  },
+});
