857ca32586591870ff3f90c5f9c28cc08209ea24
eslleyrubem <rubem.eslley@fator3tech.com.br>
2025-04-05 14:15:05 -0300

    feat: add is blocked props on button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -8,16 +8,23 @@ interface ButtonProps extends RawButtonProps {
   variant?: "text" | "outlined" | "contained";
   color?: "primary" | "secondary" | "error" | "info" | "success" | "warning";
   label: string;
+  isBlocked: boolean;
 }
 
 export const Button: React.FC<ButtonProps> = ({
   variant = "contained",
   color = "primary",
   label,
+  isBlocked,
   ...props
 }) => {
   return (
-    <RawButton variant={variant} color={color} {...props}>
+    <RawButton
+      variant={variant}
+      color={color}
+      {...props}
+      sx={isBlocked ? { opacity: 0.5, pointerEvents: "auto" } : {}}
+    >
       {label}
     </RawButton>
   );
