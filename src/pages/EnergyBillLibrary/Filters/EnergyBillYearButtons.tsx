a82fcc74b0bfbee95468e43f39a1ea7ed46a86c0
eslleyrubem <rubem.eslley@fator3tech.com.br>
2025-04-05 15:27:11 -0300

    refactor: extract filters update logic

--- src/pages/EnergyBillLibrary/Filters/EnergyBillYearButtons.tsx
+++ src/pages/EnergyBillLibrary/Filters/EnergyBillYearButtons.tsx
@@ -2,6 +2,7 @@ import { Box } from "@mui/material";
 import { Button } from "../../../components/Button/Button";
 import { years } from "../../../utils/date";
 import { GetEnergyBillsInput } from "../../../api/types/energyBill";
+import { useCallback } from "react";
 
 interface EnergyBillYearButtonsProps {
   filters: GetEnergyBillsInput;
@@ -14,12 +15,17 @@ export const EnergyBillYearButtons = ({
 }: EnergyBillYearButtonsProps) => {
   const currentYearFiltered = filters.year;
 
-  const handleChangeYear = (year: number) => {
-    setFilters((prev) => ({
-      ...prev,
-      year,
-    }));
-  };
+  const handleChangeYear = useCallback(
+    (year: number) => {
+      if (year !== filters.year) {
+        setFilters((prev) => ({
+          ...prev,
+          year,
+        }));
+      }
+    },
+    [filters.year, setFilters]
+  );
 
   return (
     <Box sx={{ display: "flex", flexWrap: "wrap", gap: 2 }}>
