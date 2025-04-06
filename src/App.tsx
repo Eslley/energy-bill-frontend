6948597cf97871851e667e3fb4e512cf23c2a090
eslleyrubem <rubem.eslley@fator3tech.com.br>
2025-04-06 18:04:09 -0300

    feat: add nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,13 @@ import { Container } from "@mui/material";
 import { DashboardPage } from "./pages/Dashboard/DashboardPage";
 import { EnergyBillLibraryPage } from "./pages/EnergyBillLibrary/EnergyBillLibraryPage";
 import { NotFound } from "./pages/NotFound/NotFound";
+import Navbar from "./components/NavBar/NavBar";
 
 const App: React.FC = () => {
   return (
     <Router>
+      <Navbar />
+
       <Container sx={{ height: "100%", mt: "100px", pb: "1em" }}>
         <Routes>
           <Route path="/" element={<DashboardPage />} />
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,150 @@
+import {
+  AppBar,
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Typography,
+  Button,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { ThemeColors } from "../../styles/theme";
+
+interface NavItem {
+  text: string;
+  key: string;
+}
+
+interface NavbarProps {
+  window?: () => Window;
+}
+
+const navItems: NavItem[] = [
+  { text: "Dashboard", key: "" },
+  { text: "Biblioteca de Faturas", key: "bills-library" },
+];
+
+export default function Navbar({ window }: NavbarProps) {
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const [currentPage, setCurrentPage] = useState<string>("");
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prev) => !prev);
+  };
+
+  const handleNavClick = (key: string) => {
+    setCurrentPage(key);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        Faturas de Energia
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <Link
+            key={item.key}
+            to={`/${item.key}`}
+            style={{ textDecoration: "none" }}
+          >
+            <ListItem disablePadding>
+              <ListItemButton
+                sx={{ textAlign: "center", color: ThemeColors.textPrimary }}
+              >
+                <ListItemText primary={item.text} />
+              </ListItemButton>
+            </ListItem>
+          </Link>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="relative" component="nav">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="Abrir menu"
+            onClick={handleDrawerToggle}
+            sx={{ mr: "-16px", display: { md: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            sx={{
+              flexGrow: 1,
+              display: { md: "block" },
+              textAlign: { xs: "center", md: "start" },
+            }}
+          >
+            <Link
+              to="/"
+              onClick={() => handleNavClick("")}
+              style={{ textDecoration: "none", color: ThemeColors.textPrimary }}
+            >
+              Faturas de Energia
+            </Link>
+          </Typography>
+          <Box sx={{ display: { xs: "none", md: "block" } }}>
+            {navItems.map((item) => (
+              <Link
+                key={item.key}
+                to={`/${item.key}`}
+                onClick={() => handleNavClick(item.key)}
+                style={{ textDecoration: "none" }}
+              >
+                <Button
+                  sx={{
+                    color: ThemeColors.textPrimary,
+                    mr: 2,
+                    backgroundColor:
+                      item.key === currentPage
+                        ? ThemeColors.primaryLight
+                        : "transparent",
+                    ":hover": {
+                      backgroundColor: ThemeColors.primary,
+                    },
+                  }}
+                >
+                  {item.text}
+                </Button>
+              </Link>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{ keepMounted: true }}
+          sx={{
+            display: { md: "block" },
+            "& .MuiDrawer-paper": { boxSizing: "border-box", width: 240 },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
+  );
+}
